f9db9a0ee8d45c8a7924fdf90ed2ed8f47b584a6
buiatuan <devils2tuan@gmail.com>
2022-03-06 14:12:28 +0700

    up  file imgs, bootstrap fontasesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {HomeComponent} from "./home/home.component";
+import {ListProductComponent} from "./list-product/listproduct.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListProductComponent,
+
   ],
   imports: [
     BrowserModule
